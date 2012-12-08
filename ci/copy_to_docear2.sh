450ba17ad48151b59958aea22eea71991180eaa8
Michael Schleichardt <michael@schleichardt.net>
2012-12-08 18:44:29 +0100

    add ci shell script to copy artifact to docear2.f4.htw-berlin.de

---
+++ ci/copy_to_docear2.sh
@@ -0,0 +1,4 @@
+remoteFolder=local@docear2.f4.htw-berlin.de:/local_home/docear
+
+scp docear_framework/dist/docear_linux.tar.gz ${remoteFolder}
+scp ci/docear2/run.sh ${remoteFolder}
---
+++ ci/docear2/run.sh
@@ -0,0 +1,4 @@
+rm docear-* && \
+tar xzf docear_linux.tar.gz && \
+cd docear-* && \
+bash docear.sh
