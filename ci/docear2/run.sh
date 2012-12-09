c90a6a4d3e9d99f5383544a74d253e475a4ac9e8
Michael Schleichardt <michael@schleichardt.net>
2012-12-09 11:43:24 +0100

    configured run script for server docear2

--- ci/docear2/run.sh
+++ ci/docear2/run.sh
@@ -1,4 +1,5 @@
+killall Xvfb; #kills docear
 rm -r --force docear-* && \
 tar xzf docear_linux.tar.gz && \
 cd docear-* && \
-bash docear.sh
+xvfb-run --auto-servernum -s "-screen 6 1280x1024x24" bash docear.sh
