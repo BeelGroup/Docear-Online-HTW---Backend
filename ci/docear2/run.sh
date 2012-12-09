1eb8af0cdad261c2b0c26775478cb87fd6dc7607
Michael Schleichardt <michael@schleichardt.net>
2012-12-09 12:17:26 +0100

    improved logging run script for docear2

--- ci/docear2/run.sh
+++ ci/docear2/run.sh
@@ -1,5 +1,11 @@
-killall Xvfb; #kills docear
+#this file will be copied to docear2
+
+echo "killing docear";
+killall Xvfb 2> /dev/null || true; #kills docear, error messages will be discarded
+echo "removing previous installation"
 rm -r --force docear-* && \
 tar xzf docear_linux.tar.gz && \
 cd docear-* && \
+rm -f nohup.out && \
+echo "starting docear" && \
 nohup xvfb-run --auto-servernum -s "-screen 6 1280x1024x24" bash docear.sh &
