9dca42555a16bf192aca7126825c286a446090a1
Michael Schleichardt <michael@schleichardt.net>
2012-12-09 11:55:52 +0100

    configured run script for server docear2: using nohup to be able to exit ssh session and the program stil runs

--- ci/docear2/run.sh
+++ ci/docear2/run.sh
@@ -2,4 +2,4 @@ killall Xvfb; #kills docear
 rm -r --force docear-* && \
 tar xzf docear_linux.tar.gz && \
 cd docear-* && \
-xvfb-run --auto-servernum -s "-screen 6 1280x1024x24" bash docear.sh
+nohup xvfb-run --auto-servernum -s "-screen 6 1280x1024x24" bash docear.sh &
