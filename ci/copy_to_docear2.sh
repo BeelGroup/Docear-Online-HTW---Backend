7f9cf438b0e1b7a9c4864fad6e0515cbab203882
Michael Schleichardt <michael@schleichardt.net>
2012-12-10 22:59:47 +0100

    added scripts for docear config in docear2

--- ci/copy_to_docear2.sh
+++ ci/copy_to_docear2.sh
@@ -2,3 +2,4 @@ remoteFolder=local@docear2.f4.htw-berlin.de:/local_home/docear
 
 scp docear_framework/dist/docear_linux.tar.gz ${remoteFolder}
 scp ci/docear2/run.sh ${remoteFolder}
+scp -r ci/docear2/docear-settings-folder/ ${remoteFolder}
--- ci/docear2/run.sh
+++ ci/docear2/run.sh
@@ -1,9 +1,16 @@
 #this file will be copied to docear2
 
+workspace_folder=~/docear-ci-workspace
+docear_config_folder=~/.docear
+
 echo "killing docear";
 killall Xvfb 2> /dev/null || true; #kills docear, error messages will be discarded
 echo "removing previous installation"
-rm -r --force docear-* && \
+rm -r --force docear-*-build* && \
+rm -r --force ${workspace_folder} && \
+mkdir -p ${workspace_folder} && \
+cp ~/docear/docear-settings-folder/auto.mmfilter ${docear_config_folder} && \
+cp ~/docear/docear-settings-folder/auto.properties ${docear_config_folder} && \
 tar xzf docear_linux.tar.gz && \
 cd docear-* && \
 rm -f nohup.out && \
