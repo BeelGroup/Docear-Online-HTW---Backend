bc656256bbe57f61dc3610130c617810c6ddb166
Julius S <mail@julius-seltenheim.com>
2013-01-04 00:10:20 +0100

    freeplane version of plugin

--- docear_plugin_webservice/src/org/docear/plugin/webservice/Activator.java
+++ docear_plugin_webservice/src/org/docear/plugin/webservice/Activator.java
@@ -15,15 +15,8 @@ public class Activator extends DocearService implements BundleActivator {
 
 	//private ServiceTracker httpServiceTracker;
 
-	public void stop(BundleContext context) throws Exception {
-//		httpServiceTracker.close();
-//		httpServiceTracker = null;
-	}
 
 	public void startService(BundleContext context, ModeController modeController) {
-//		httpServiceTracker = new HttpServiceTracker(context);
-//		httpServiceTracker.open();
-		
 		new WebserviceController(modeController,context);
 	}
 
@@ -31,48 +24,13 @@ public class Activator extends DocearService implements BundleActivator {
 		return null;
 	}
 
-//	private class HttpServiceTracker extends ServiceTracker {
-//
-//		public HttpServiceTracker(BundleContext context) {
-//			super(context, HttpService.class.getName(), null);
-//		}
-//
-//		public Object addingService(ServiceReference reference) {
-//			HttpService httpService = (HttpService) context.getService(reference);
-//			try {
-//				Thread currentThread = Thread.currentThread();
-//				ClassLoader cl = currentThread.getContextClassLoader();
-//				Thread.currentThread().setContextClassLoader(HttpServiceTracker.class.getClassLoader());
-//				                        
-//				HttpContext httpContext = httpService.createDefaultHttpContext();   
-//				Hashtable params = new Hashtable();
-//				params.put("javax.ws.rs.Application",
-//				"com.servicemesh.agility.internal.api.v1_0.AgilityApplication");
-//				httpService.registerServlet("/api/v1.0", new ServletWrapper(new
-//				com.sun.jersey.spi.container.servlet.ServletContainer()), params,
-//				httpContext);
-//
-//				params = new Hashtable();
-//				params.put("javax.ws.rs.Application",
-//				"com.servicemesh.agility.internal.api.RootApplication");
-//				httpService.registerServlet("/api", new ServletWrapper(new
-//				com.sun.jersey.spi.container.servlet.ServletContainer()), params,
-//				httpContext);
-//				currentThread.setContextClassLoader(cl);
-//				httpService.registerResources("/helloworld.html", "/helloworld.html", null); //$NON-NLS-1$ //$NON-NLS-2$
-//				httpService.registerServlet("/helloworld", new HelloWorldServlet(), null, null); //$NON-NLS-1$
-//			} catch (Exception e) {
-//				e.printStackTrace();
-//			}
-//			return httpService;
-//		}		
-//
-//		public void removedService(ServiceReference reference, Object service) {
-//			HttpService httpService = (HttpService) service;
-//			httpService.unregister("/helloworld.html"); //$NON-NLS-1$
-//			httpService.unregister("/helloworld"); //$NON-NLS-1$
-//			super.removedService(reference, service);
-//		}
+//	@Override
+//	public void start(BundleContext context) throws Exception {
+//		final ModeController modeController = Controller.getCurrentModeController();
+//		new WebserviceController(modeController,context);
 //	}
 
+	@Override
+	public void stop(BundleContext arg0) throws Exception {		
+	}
 }
