97397afe21bdac80b670c94b9057773377788615
Michael Schleichardt <michael@schleichardt.net>
2013-01-09 16:45:28 +0100

    changed ci-build from docear_plugin_webservice to freeplane_plugin_webservice

--- ci/build.sh
+++ ci/build.sh
@@ -3,7 +3,7 @@
 set -x
 
 ant_file_name=build_docear.xml
-ant_file_src="docear_plugin_webservice/ant/${ant_file_name}"
+ant_file_src="freeplane_plugin_webservice/ant/${ant_file_name}"
 ant_file_target_folder=docear_framework/ant
 
 cp ${ant_file_src} ${ant_file_target_folder}
